62faee1fa555d137440bef480d5c3b014e58c3e2
RabilJ <jakub.popielarski7@gmail.com>
2018-11-09 18:31:37 +0100

    przyklad

---
+++ src/main/java/com/example/demo/controller/RandomNumberController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.RandomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Random;
+
+@Controller
+public class RandomNumberController {
+    private RandomService randomService;
+@Autowired
+    public RandomNumberController(RandomService randomService) {
+        this.randomService = randomService;
+    }
+
+    @GetMapping("/small")
+    @ResponseBody
+    public String smallRandom() {
+        int smallInt = randomService.getSmallInt();
+        return "Losowa liczba: " + smallInt;
+    }
+
+    @GetMapping("/big")
+    @ResponseBody
+    public String bigRandom() {
+        int bigInt = randomService.getBigInt();
+        return "Losowa liczba: " + bigInt;
+    }
+}
